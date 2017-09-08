c743d120c1d037821acbe670d732f1e35153f9d4
Akshar Dave <akshar314@gmail.com>
2017-09-08 19:24:49 +0530

    add fk reln to posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -15,8 +15,11 @@ describe 'navigate' do
 
   describe "creation" do
     before do
+      user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+      login_as(user, :scope => :user)
       visit new_post_path
     end
+
     it "should have a new form" do
       expect(page.status_code).to eq(200)
     end
@@ -27,6 +30,12 @@ describe 'navigate' do
       click_on "Save"
       expect(page).to have_content("random")
     end
-  end
 
+    it "can associate user with a post" do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
+      expect(User.last.posts.last.rationale).to have_content("User Association")
+    end
+  end
 end
