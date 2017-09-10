03537624fc6c3f6388744a68291c3f90147d0884
Akshar Dave <akshar314@gmail.com>
2017-09-11 00:46:51 +0530

    Edit post functionality

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -30,7 +30,7 @@ describe 'navigate' do
   describe "creation" do
     before do
       user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-       login_as(user, :scope => :user)
+      login_as(user, :scope => :user)
       visit new_post_path
     end
 
@@ -50,6 +50,25 @@ describe 'navigate' do
       fill_in 'post[rationale]', with: "User Association"
       click_on "Save"
       expect(User.last.posts.last.rationale).to have_content("User Association")
+    end
+  end
+
+  describe "edit" do
+    before do
+      @post = FactoryGirl.create(:post)
+    end
+    it "can be reached by clicking index on edit page" do
+      visit posts_path
+      click_link "edit_#{@post.id}"
+      expect(page.status_code).to eq(200)
+    end
+
+    it "can be edited" do
+      visit edit_post_path(@post)
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "edited content"
+      click_on "Save"
+      expect(page).to have_content("edited content")
     end
   end
 end
