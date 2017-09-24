5d26d570621d941df3756e73478f6d0a579fe1ef
Akshar Dave <akshar314@gmail.com>
2017-09-24 19:07:25 +0530

    edit functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 @user = User.create(email: "foo@bar.com", first_name: "Foo", last_name: "Bar", password: "foo@bar", password_confirmation: "foo@bar")
 
 puts "1 user created!"
+
+@user = AdminUser.create(email: "foo1@bar.com", first_name: "Jesse", last_name: "Pinkman", password: "foo1@bar", password_confirmation: "foo1@bar")
+
+puts "1 Adminuser created!"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
