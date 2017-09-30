bc19935269a936a73a1d2e51b5da0bc20888d2f5
Akshar Dave <akshar314@gmail.com>
2017-10-01 00:47:07 +0530

    lock post once approved

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,7 +1,21 @@
 class PostPolicy < ApplicationPolicy
   def update?
-    record.user_id == user.id || user.type =='AdminUser'
+    return true if !post_approved? && admin_or_user
+    return true if admin? && post_approved?
+
   end
 
+  private
 
+  def admin_or_user
+    record.user_id == user.id || admin?
+  end
+
+  def admin?
+    user.type =='AdminUser'
+  end
+
+  def post_approved?
+    record.approved?
+  end
 end
