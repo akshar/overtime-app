037a90bb52504fd7bc54abcbe769f24f0a9d60be
Akshar Dave <akshar314@gmail.com>
2017-10-01 15:20:02 +0530

    Add overtime hrs requested

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ puts "1 user created!"
 puts "1 Adminuser created!"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts created"
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     date Date.today
     rationale "Some Rationale"
     user
+    overtime_request 3.5
   end
 
   factory :second_post, class: "Post" do
@@ -15,5 +16,6 @@ FactoryGirl.define do
     date Date.yesterday
     rationale "Some more content"
     non_authorized_user
+    overtime_request 0.5
   end
 end
