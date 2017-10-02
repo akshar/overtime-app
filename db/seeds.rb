d027dabf650a34e64ea078c363ea4ea49836339a
Akshar Dave <akshar314@gmail.com>
2017-10-03 00:02:29 +0530

    updated audit_log factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,4 +20,7 @@ puts "1 Adminuser created!"
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
-puts "100 posts created"
+100.times do |aduit_log|
+  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+end
+puts "100 aduit logs created"
