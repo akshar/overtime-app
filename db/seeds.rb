4bf8ba9c735b66111468b3a9eb2f6472509e801c
Akshar Dave <akshar314@gmail.com>
2017-10-03 00:36:04 +0530

    Validations & defaults for audit_log

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 
 puts "1 user created!"
 
-@user = AdminUser.create(email: "foo1@bar.com",
-                         first_name: "Jesse",
-                         last_name: "Pinkman",
-                         password: "foo1@bar",
-                         password_confirmation: "foo1@bar",
-                         phone: "1234567890")
+AdminUser.create(email: "foo1@bar.com",
+                 first_name: "Jesse",
+                 last_name: "Pinkman",
+                 password: "foo1@bar",
+                 password_confirmation: "foo1@bar",
+                 phone: "1234567890")
 
 puts "1 Adminuser created!"
 
@@ -21,6 +21,6 @@ puts "1 Adminuser created!"
 end
 
 100.times do |aduit_log|
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 puts "100 aduit logs created"
