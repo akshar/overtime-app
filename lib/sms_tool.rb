d4a737bcf7024df05f17cd12ae58511c3a022b05
Akshar Dave <akshar314@gmail.com>
2017-10-02 22:59:35 +0530

    Integrate Twilio sms notificaiton

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -1,6 +1,13 @@
 module SmsTool
-  def self.send_sms(num, msg)
-    puts "sending sms.."
-    puts "#{msg} to #{num}"
+  account_sid = Rails.application.secrets.twilio['TWILIO_ACCOUNT_SID']
+  auth_token =  Rails.application.secrets.twilio['TWILIO_AUTH_TOKEN']
+  @client = Twilio::REST::Client.new account_sid, auth_token
+
+  def self.send_sms(number, message)
+    @client.messages.create(
+      from: Rails.application.secrets.twilio['TWILIO_PHONE_NUMBER'],
+      to: "+91#{number}",
+      body: "#{message}"
+    )
   end
 end
