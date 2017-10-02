68b0ec481025bcf9c1a9b7726b4785c1c051985e
Akshar Dave <akshar314@gmail.com>
2017-10-02 22:51:56 +0530

    regex for phone numbers, add spec

--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -1,7 +1,8 @@
 namespace :notification do
   desc "send sms notification to employees asking them to log overtime"
+
   task sms: :environment do
-  	puts "log time"
+
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,20 @@ RSpec.describe User, type: :model do
     end
   end
 
+  describe "validations" do
+    it "requires for phone attribute to have only 10 character" do
+       @user.phone = "123456789"
+      expect(@user).to_not be_valid
+    end
+
+    it "requires phone attribute to be only numbers" do
+      @user.phone = "0123456789"
+      expect(@user).to be_valid
+    end
+  end
+
+
+
   describe "custom user methods" do
     it "should have full name" do
       expect(@user.full_name).to eq("PINKMAN, JESSE")
