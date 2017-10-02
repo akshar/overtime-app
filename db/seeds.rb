9323bd60c9007e1cb928b880bc6aac0f096a144e
Akshar Dave <akshar314@gmail.com>
2017-10-02 11:50:17 +0530

    Add phone attribute to Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
-@user = User.create(email: "foo@bar.com", first_name: "Foo", last_name: "Bar", password: "foo@bar", password_confirmation: "foo@bar")
+@user = User.create(email: "foo@bar.com",
+                    first_name: "Foo",
+                    last_name: "Bar",
+                    password: "foo@bar",
+                    password_confirmation: "foo@bar",
+                    phone: "1234567890")
 
 puts "1 user created!"
 
-@user = AdminUser.create(email: "foo1@bar.com", first_name: "Jesse", last_name: "Pinkman", password: "foo1@bar", password_confirmation: "foo1@bar")
+@user = AdminUser.create(email: "foo1@bar.com",
+                         first_name: "Jesse",
+                         last_name: "Pinkman",
+                         password: "foo1@bar",
+                         password_confirmation: "foo1@bar",
+                         phone: "1234567890")
 
 puts "1 Adminuser created!"
 
