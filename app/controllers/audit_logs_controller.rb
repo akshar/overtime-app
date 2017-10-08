f9a43014d2a8361a18c85fd64b61c82c640f79ef
Akshar Dave <akshar314@gmail.com>
2017-10-08 15:37:23 +0530

    Pagination for Posts & AuditLog

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -1,6 +1,6 @@
 class AuditLogsController < ApplicationController
   def index
-    @audit_log = AuditLog.all
+    @audit_log = AuditLog.page(params[:page]).per(10)
     authorize @audit_log
   end
 
